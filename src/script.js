2ade8e4d24641a2910cb3600cae5134e788bbd85
sunDalik <dali1999@mail.ru>
2019-10-19 17:52:56 +0300

    Tile movement

---
+++ src/script.js
@@ -0,0 +1,109 @@
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite,
+    TextureCache = PIXI.utils.TextureCache;
+
+let triangle;
+const tileSize = 75;
+
+//Creating app
+let app = new Application();
+app.renderer.backgroundColor = 0xBB00BB;
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+document.body.appendChild(app.view);
+
+//Adding sprites to stage
+loader
+    .add("src/images/player.png")
+    .on("progress", loadProgressHandler)
+    .load(setup);
+
+let gameMap = new PIXI.Graphics();
+for (let y = 0; y < app.view.height; y += tileSize) {
+    for (let x = 0; x < app.view.width; x += tileSize) {
+        gameMap.lineStyle(1, 0x000000);
+        gameMap.drawRect(x, y, tileSize, tileSize);
+    }
+}
+app.stage.addChild(gameMap);
+
+function loadProgressHandler(loader, resource) {
+    console.log("Loading resource: " + resource.url);
+    console.log("Progress: " + loader.progress + "%");
+}
+
+function setup() {
+    triangle = new Sprite(resources["src/images/player.png"].texture);
+    app.stage.addChild(triangle);
+    triangle.scale.set(tileSize / triangle.width - 0.25, tileSize / triangle.height - 0.25);
+    triangle.position.set(tileSize * 6 + (tileSize - triangle.width)/2, tileSize * 4 + (tileSize - triangle.height)/2);
+    app.ticker.add(delta => gameLoop(delta));
+    const wKey = keyboard("w");
+    const aKey = keyboard("a");
+    const sKey = keyboard("s");
+    const dKey = keyboard("d");
+
+    wKey.press = () => {
+        triangle.y -= tileSize;
+    };
+    aKey.press = () => {
+        triangle.x -= tileSize;
+    };
+    sKey.press = () => {
+        triangle.y += tileSize;
+    };
+    dKey.press = () => {
+        triangle.x += tileSize;
+    };
+}
+
+function gameLoop(delta) {
+
+}
+
+function keyboard(value) {
+    let key = {
+        value: value,
+        isDown: false,
+        isUp: true,
+        press: undefined,
+        release: undefined
+    };
+
+    key.downHandler = event => {
+        if (event.key === key.value) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+            event.preventDefault();
+        }
+    };
+
+    key.upHandler = event => {
+        if (event.key === key.value) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+            event.preventDefault();
+        }
+    };
+
+    const downListener = key.downHandler.bind(key);
+    const upListener = key.upHandler.bind(key);
+
+    window.addEventListener("keydown", downListener, false);
+    window.addEventListener("keyup", upListener, false);
+
+    // Detach event listeners
+    key.unsubscribe = () => {
+        window.removeEventListener("keydown", downListener);
+        window.removeEventListener("keyup", upListener);
+    };
+
+    return key;
+}
