3f5d81f4ea5da0a51d6e60abbfb8a9f3c8330df9
sunDalik <dali1999@mail.ru>
2019-10-21 00:01:57 +0300

    Add roller animation

--- src/js/classes/roller.js
+++ src/js/classes/roller.js
@@ -3,13 +3,21 @@ class Roller extends Enemy {
         super(texture, tilePositionX, tilePositionY);
         this.health = 100;
         this.direction = 1;
+        this.animation = null;
     }
 
     move(gameMap) {
+        let frequency = 15;
+        let counter = 0;
+        let roller = this;
+        const step = tileSize / frequency;
+        clearTimeout(this.animation);
+        this.place(tileSize);
+
         gameMap[this.tilePosition.y][this.tilePosition.x] = "";
         if (isNotAWall(gameMap, this.tilePosition.x + this.direction, this.tilePosition.y)) {
             this.tilePosition.x += this.direction;
-            this.place(tileSize);
+            animate();
         } else {
             this.direction *= -1;
             this.setAnchorToCenter();
@@ -17,5 +25,18 @@ class Roller extends Enemy {
             this.place(tileSize);
         }
         gameMap[this.tilePosition.y][this.tilePosition.x] = "r";
+
+        function animate() {
+            roller.animation = setTimeout(() => {
+                roller.position.x += step * roller.direction;
+                counter++;
+                if (counter < frequency) animate();
+                else {
+                    roller.animation = null;
+                    roller.place(tileSize);
+                }
+            }, 2)
+        }
     }
+
 }
