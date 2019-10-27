d15e141b17b6f49113f9ca86c8f5b0a8439714fd
sunDalik <dali1999@mail.ru>
2019-10-27 18:28:15 +0300

    Generate paths

--- src/js/level_generation.js
+++ src/js/level_generation.js
@@ -59,10 +59,60 @@ function generateLevel() {
         if (currentRoom.length + randomOffsetY > previousYMaxAddition) previousYMaxAddition = currentRoom.length + randomOffsetY;
     }
 
+    let entryPoints = [];
+    for (let i = 0; i < level.length; ++i) {
+        for (let j = 0; j < level[0].length; ++j) {
+            if (level[i][j] === "entry") entryPoints.push({coords: {y: i, x: j}, connected: false});
+        }
+    }
+
+    //temporary solution, in future I will add 1 more entry point to starting location
+    if (entryPoints.length % 2 === 1) entryPoints.pop();
+
+    // the Graph class is weird, levelGraph.grid.length will return number of Xs and levelGraph.grid[0].length number of Ys
+    let levelGraph = new Graph(level);
+    for (let i = 0; i < levelGraph.grid.length; ++i) {
+        for (let j = 0; j < levelGraph.grid[0].length; ++j) {
+            if (levelGraph.grid[i][j].weight === "v" || levelGraph.grid[i][j].weight === "entry") {
+                levelGraph.grid[i][j].weight = 1;
+            } else {
+                levelGraph.grid[i][j].weight = 0;
+            }
+        }
+    }
+
+    for (const entry of entryPoints) {
+        let possibleConnections = [];
+        for (const entry2 of entryPoints) {
+            if (!entry2.connected) {
+                const start = levelGraph.grid[entry.coords.y][entry.coords.x];
+                const end = levelGraph.grid[entry2.coords.y][entry2.coords.x];
+                const result = astar.search(levelGraph, start, end);
+                possibleConnections.push({connection: result, entry: entry2});
+            }
+        }
+
+        let minConnectionIndex;
+        for (let i = 1; i < possibleConnections.length; ++i) {
+            if (minConnectionIndex === undefined) {
+                if (possibleConnections[i].connection.length !== 0) {
+                    minConnectionIndex = i;
+                }
+            } else if (possibleConnections[i].connection.length < possibleConnections[minConnectionIndex].connection.length
+                && possibleConnections[i].connection.length !== 0) {
+                minConnectionIndex = i;
+            }
+        }
+
+        if (minConnectionIndex !== undefined) {
+            possibleConnections[minConnectionIndex].entry.connected = true;
+            entry.connected = true;
+            level = drawConnection(level, possibleConnections[minConnectionIndex].connection);
+        }
+    }
     return level;
 }
 
-// sometimes exceptions
 function mergeRoomIntoLevel(level, room, startX, startY) {
     for (let i = startY; i < startY + room.length; ++i) {
         for (let j = startX; j < startX + room[0].length; ++j) {
@@ -70,5 +120,13 @@ function mergeRoomIntoLevel(level, room, startX, startY) {
         }
     }
 
+    return level;
+}
+
+function drawConnection(level, connection) {
+    //[x][y] instead [y][x] because once again the Graph is weird
+    for (let i = 0; i < connection.length; ++i) {
+        level[connection[i].x][connection[i].y] = "";
+    }
     return level;
 }
