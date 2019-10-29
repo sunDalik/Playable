e220ae3c7c0f412b56218ddaf31c28f1b70c2913
sunDalik <dali1999@mail.ru>
2019-10-29 19:54:15 +0300

    Improve stars behavior
    - They have 2 hp (doesn't matter yet though)
    - Have 1 turn delay
    - Can attack both cardinally and diagonally (depends on the star)

--- src/js/classes/enemies/star.js
+++ src/js/classes/enemies/star.js
@@ -3,28 +3,45 @@
 class Star extends Enemy {
     constructor(tilePositionX = 0, tilePositionY = 0, texture = GameState.resources["src/images/enemies/star.png"].texture) {
         super(texture, tilePositionX, tilePositionY);
-        this.health = 100;
+        this.health = 2;
         this.triggered = false;
+        this.triggeredDirections = null;
         this.SHAKE_ANIMATION_TIME = 4;
         this.entityType = ENEMY_TYPE.STAR;
+        this.turnDelay = 0;
     }
 
     move() {
-        if (this.triggered) this.attack();
-        else {
-            loop: for (let offset = -2; offset <= 2; offset++) {
-                for (let sign = -1; sign <= 1; sign += 2) {
-                    if (offset !== 0) {
-                        const player = getPlayerOnTile(this.tilePosition.x + offset, this.tilePosition.y + offset * sign);
-                        if (player !== null) {
-                            this.triggered = true;
-                            break loop;
+        if (this.turnDelay === 0) {
+            if (this.triggered) this.attack();
+            else {
+                loop: for (let offset = -2; offset <= 2; offset++) {
+                    for (let sign = -1; sign <= 1; sign += 2) {
+                        if (offset !== 0) {
+                            const player = getPlayerOnTile(this.tilePosition.x + offset, this.tilePosition.y + offset * sign);
+                            if (player !== null) {
+                                this.triggered = true;
+                                this.triggeredDirections = DIRECTIONS.DIAGONAL;
+                                break loop;
+                            }
                         }
                     }
                 }
+                loop2: for (let x = -1; x <= 1; x++) {
+                    for (let y = -1; y <= 1; y++) {
+                        if (Math.abs(x) !== Math.abs(y)) {
+                            const player = getPlayerOnTile(this.tilePosition.x + x, this.tilePosition.y + y);
+                            if (player !== null) {
+                                this.triggered = true;
+                                this.triggeredDirections = DIRECTIONS.CARDINAL;
+                                break loop2;
+                            }
+                        }
+                    }
+                }
+                if (this.triggered) this.shake();
             }
-            if (this.triggered) this.shake();
-        }
+        } else this.turnDelay--;
     }
 
     shake() {
@@ -49,16 +66,31 @@ class Star extends Enemy {
 
     attack() {
         this.triggered = false;
-        for (let offset = -2; offset <= 2; offset++) {
-            for (let sign = -1; sign <= 1; sign += 2) {
-                const attackPositionX = this.tilePosition.x + offset;
-                const attackPositionY = this.tilePosition.y + offset * sign;
-                if (offset !== 0 && isNotAWall(attackPositionX, attackPositionY) && !isEnemy(attackPositionX, attackPositionY)) {
-                    createFadingAttack(new TileElement(GameState.resources["src/images/enemy_attack.png"].texture, attackPositionX, attackPositionY));
-                    const player = getPlayerOnTile(attackPositionX, attackPositionY);
-                    if (player !== null) damagePlayer(player, 1);
-                }
-            }
+        if (this.triggeredDirections === DIRECTIONS.CARDINAL) {
+            this.attackTileAtOffset(0, 1);
+            this.attackTileAtOffset(1, 0);
+            this.attackTileAtOffset(0, -1);
+            this.attackTileAtOffset(-1, 0);
+        } else if (this.triggeredDirections === DIRECTIONS.DIAGONAL) {
+            this.attackTileAtOffset(-2, -2);
+            this.attackTileAtOffset(-1, -1);
+            this.attackTileAtOffset(2, 2);
+            this.attackTileAtOffset(1, 1);
+            this.attackTileAtOffset(-2, 2);
+            this.attackTileAtOffset(-1, 1);
+            this.attackTileAtOffset(2, -2);
+            this.attackTileAtOffset(1, -1);
+        }
+        this.turnDelay = 1;
+    }
+
+    attackTileAtOffset(tileOffsetX, tileOffsetY) {
+        const attackPositionX = this.tilePosition.x + tileOffsetX;
+        const attackPositionY = this.tilePosition.y + tileOffsetY;
+        if (isNotAWall(attackPositionX, attackPositionY) && !isEnemy(attackPositionX, attackPositionY)) {
+            createFadingAttack(new TileElement(GameState.resources["src/images/enemy_attack.png"].texture, attackPositionX, attackPositionY));
+            const player = getPlayerOnTile(attackPositionX, attackPositionY);
+            if (player !== null) damagePlayer(player, 1);
         }
     }
 }
--- src/js/classes/enemies/star_b.js
+++ src/js/classes/enemies/star_b.js
@@ -3,29 +3,46 @@
 class StarB extends Star {
     constructor(tilePositionX = 0, tilePositionY = 0, texture = GameState.resources["src/images/enemies/star_b.png"].texture) {
         super(tilePositionX, tilePositionY, texture);
-        this.health = 100;
         this.entityType = ENEMY_TYPE.STAR_B;
     }
 
     move() {
-        if (this.triggered) this.attack();
-        else {
-            loop: for (let x = 0; x <= 3; x++) {
-                for (let y = 0; y <= 3; y++) {
-                    for (let signX = -1; signX <= 1; signX += 2) {
-                        for (let signY = -1; signY <= 1; signY += 2) {
-                            if (!(x === 3 && y === 3) && Math.abs(x - y) <= 1) {
-                                if (getPlayerOnTile(this.tilePosition.x + x * signX, this.tilePosition.y + y * signY)) {
-                                    this.triggered = true;
-                                    break loop;
+        if (this.turnDelay === 0) {
+            if (this.triggered) this.attack();
+            else {
+                loop: for (let x = 0; x <= 3; x++) {
+                    for (let y = 0; y <= 3; y++) {
+                        for (let signX = -1; signX <= 1; signX += 2) {
+                            for (let signY = -1; signY <= 1; signY += 2) {
+                                if (!(x === 3 && y === 3) && Math.abs(x - y) <= 1) {
+                                    if (getPlayerOnTile(this.tilePosition.x + x * signX, this.tilePosition.y + y * signY)) {
+                                        this.triggered = true;
+                                        break loop;
+                                    }
                                 }
                             }
                         }
                     }
                 }
+                if (this.triggered) this.shake();
             }
-            if (this.triggered) this.shake();
-        }
+        } else this.turnDelay--;
     }
 
+    attack() {
+        this.triggered = false;
+        this.attackTileAtOffset(-2, -2);
+        this.attackTileAtOffset(-1, -1);
+        this.attackTileAtOffset(2, 2);
+        this.attackTileAtOffset(1, 1);
+        this.attackTileAtOffset(-2, 2);
+        this.attackTileAtOffset(-1, 1);
+        this.attackTileAtOffset(2, -2);
+        this.attackTileAtOffset(1, -1);
+        this.attackTileAtOffset(0, 1);
+        this.attackTileAtOffset(1, 0);
+        this.attackTileAtOffset(0, -1);
+        this.attackTileAtOffset(-1, 0);
+        this.turnDelay = 1;
+    }
 }
