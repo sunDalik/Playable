5e1d499c10f7df8562ebdf5a32e28f59cdd923c3
sunDalik <dali1999@mail.ru>
2019-11-04 20:45:58 +0300

    Fix ninja knife movement

--- src/js/classes/equipment/ninja_knife.js
+++ src/js/classes/equipment/ninja_knife.js
@@ -13,8 +13,10 @@ class NinjaKnife {
             this.createNinjaKnifeAnimation(wielder.tilePosition.x, wielder.tilePosition.y, attackTileX, attackTileY);
             attackTile(attackTileX, attackTileY, atk, tileDirX, tileDirY, this);
             if (isNotAWallOrEnemy(wielder.tilePosition.x + tileDirX * 2, wielder.tilePosition.y + tileDirY * 2)) {
+                removePlayerFromGameMap(wielder);
                 if (tileDirX !== 0) wielder.slideX(tileDirX * 2, 4);
-                else if (tileDirY !== 0) wielder.slideY(tileDirY * 2, 4);
+                else wielder.slideY(tileDirY * 2, 4);
+                placePlayerOnGameMap(wielder);
                 if (Game.gameMap[attackTileY][attackTileX].entity) Game.gameMap[attackTileY][attackTileX].entity.stun = 1;
             }
             return true;
