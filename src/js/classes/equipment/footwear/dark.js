921e426d79955e718ceb437c3d73cdc500691d17
sunDalik <dali1999@mail.ru>
2019-12-03 00:00:13 +0300

    Don't apply dark boots effect if the wielder is carried

--- src/js/classes/equipment/footwear/dark.js
+++ src/js/classes/equipment/footwear/dark.js
@@ -22,6 +22,7 @@ export class DarkBoots {
     }
 
     onNewTurn(wielder) {
+        if (wielder.carried) return false;
         const hazard = Game.map[wielder.tilePosition.y][wielder.tilePosition.x].hazard;
         if (hazard) {
             if (hazard.type === HAZARD_TYPE.POISON) {
