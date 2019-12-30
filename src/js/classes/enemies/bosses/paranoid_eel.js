4155cfb01998a38ae8307897357c0c691a892c24
sunDalik <dali1999@mail.ru>
2019-12-30 22:57:28 +0300

    Add some movement to paranoid eel

--- src/js/classes/enemies/bosses/paranoid_eel.js
+++ src/js/classes/enemies/bosses/paranoid_eel.js
@@ -1,6 +1,7 @@
 import {Game} from "../../../game"
-import {ENEMY_TYPE, ROLE, STAGE} from "../../../enums";
+import {ENEMY_TYPE} from "../../../enums";
 import {Boss} from "./boss";
+import {isRelativelyEmpty} from "../../../map_checks";
 
 export class ParanoidEel extends Boss {
     constructor(tilePositionX, tilePositionY, texture = Game.resources["src/images/bosses/paranoid_eel/neutral.png"].texture) {
@@ -20,15 +21,57 @@ export class ParanoidEel extends Boss {
     }
 
     move() {
-
+        // check if empty
+        this.slide(this.direction.x, this.direction.y);
     }
 
-    fitToTile() {
-        super.fitToTile();
+    slide(tileStepX, tileStepY) {
+        this.wiggled = false;
+        super.slide(tileStepX, tileStepY, () => {
+            if (this.animationCounter >= this.SLIDE_ANIMATION_TIME / 2 && !this.wiggled) {
+                this.wiggle();
+                this.wiggled = true;
+            }
+        }, () => {
+            if (!isRelativelyEmpty(this.tilePosition.x + tileStepX + Math.sign(tileStepX),
+                this.tilePosition.y + tileStepY + Math.sign(tileStepY))) this.turnAround();
+            this.wiggled = false;
+        });
     }
 
-    place() {
-        super.place();
+    turnAround() {
+        if (this.direction.x !== 0) {
+            this.flip();
+            this.direction.x *= -1;
+        }
+    }
+
+    flip() {
+        const oldScaleX = this.scale.x;
+        const time = 12;
+        const step = -Math.sign(oldScaleX) * Math.abs(oldScaleX) * 2 / time;
+        let counter = 0;
+        const animation = delta => {
+            counter += delta;
+            this.scale.x += step * delta;
+            if (counter >= time) {
+                this.scale.x = -oldScaleX;
+                Game.app.ticker.remove(animation);
+            }
+        };
+        Game.app.ticker.add(animation);
+    }
+
+    wiggle() {
+        if (this.direction.x !== 0) {
+            if (this.texture === Game.resources["src/images/bosses/paranoid_eel/neutral.png"].texture) {
+                this.texture = Game.resources["src/images/bosses/paranoid_eel/neutral_2.png"].texture
+            } else {
+                this.texture = Game.resources["src/images/bosses/paranoid_eel/neutral.png"].texture
+            }
+        } else if (this.direction.y !== 0) {
+
+        }
     }
 
     placeOnMap() {
