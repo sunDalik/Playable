e5987aa970a2ad6c88fc5851bbdee92a7c4251bb
sunDalik <dali1999@mail.ru>
2020-01-05 14:38:28 +0300

    Refactor spiky wall trap

--- src/js/classes/enemies/spiky_wall_trap.js
+++ src/js/classes/enemies/spiky_wall_trap.js
@@ -4,6 +4,7 @@ import {ENEMY_TYPE, ROLE} from "../../enums";
 import {getPlayerOnTile} from "../../map_checks";
 import {randomChoice} from "../../utils/random_utils";
 import {getCardinalDirectionsWithNoWallsOrInanimates} from "../../utils/map_utils";
+import {FullTileElement} from "../tile_elements/full_tile_element";
 
 export class SpikyWallTrap extends Enemy {
     constructor(tilePositionX, tilePositionY, texture = Game.resources["src/images/enemies/spiky_wall_trap_x.png"].texture) {
@@ -18,54 +19,36 @@ export class SpikyWallTrap extends Enemy {
         this.movable = false;
         this.role = ROLE.WALL_TRAP;
         this.direction = {x: 1, y: 0};
-        this.zIndex = Game.primaryPlayer.zIndex + 1;
-        this.intentIcon.zIndex = this.zIndex + 1;
+        this.scaleModifier = 1;
+        this.fitToTile();
+        this.spikesSprite = new FullTileElement(Game.resources["src/images/enemies/spikes_right.png"].texture, 0, 0);
+        this.spikesSprite.zIndex = Game.primaryPlayer.zIndex + 1;
+        this.spikesSprite.visible = false;
+        Game.world.addChild(this.spikesSprite);
+        this.intentIcon2 = this.createIntentIcon();
+        this.intentIcon2.alpha = 0.8;
+        this.intentIcon2.width = this.intentIcon2.height = 15;
+        this.intentIcon.zIndex = this.intentIcon2.zIndex = Game.primaryPlayer.zIndex + 2;
     }
 
     afterMapGen() {
         const directions = getCardinalDirectionsWithNoWallsOrInanimates(this);
         if (directions.length === 0) this.die(null);
         else this.direction = randomChoice(directions);
-        this.fitToTile();
+
+        this.spikesSprite.tilePosition.x = this.tilePosition.x + this.direction.x;
+        this.spikesSprite.tilePosition.y = this.tilePosition.y + this.direction.y;
+        this.spikesSprite.place();
+        this.spikesSprite.angle = this.getArrowRightAngleForDirection(this.direction);
         this.updateTexture();
-        this.place();
-    }
-
-    fitToTile() {
-        let scaleX, scaleY;
-        if (this.getUnscaledHeight() < this.getUnscaledWidth()) {
-            scaleY = Game.TILESIZE / this.getUnscaledHeight();
-            scaleX = Math.abs(scaleY);
-        } else {
-            scaleX = Game.TILESIZE / this.getUnscaledWidth();
-            scaleY = Math.abs(scaleX);
-        }
-        this.scale.set(scaleX, scaleY);
-    }
-
-    place() {
-        super.place();
-        if (!this.direction) return;
-        this.position.x += this.width / 4 * this.direction.x;
-        this.position.y += this.height / 4 * this.direction.y;
-    }
-
-    moveHealthContainer() {
-        super.moveHealthContainer();
-        if (this.type !== ENEMY_TYPE.SPIKY_WALL_TRAP) return;
-        //dude... refactor this dude's textures already!
-        if (this.direction.y === 1) {
-            this.intentIcon.position.y = this.position.y - this.height / 4 - this.intentIcon.height / 2;
-        } else if (this.direction.y === -1) {
-            this.intentIcon.position.y = this.position.y - this.height / 4 - this.intentIcon.height / 2;
-        }
     }
 
     move() {
+        this.spikesSprite.visible = false;
         if (this.triggered) {
-            this.attack();
             this.triggered = false;
             this.currentTurnDelay = this.turnDelay;
+            this.attack();
         } else if (this.currentTurnDelay <= 0) {
             let triggerDirs;
             if (this.direction.x !== 0) {
@@ -96,29 +79,18 @@ export class SpikyWallTrap extends Enemy {
 
     attack() {
         const player = getPlayerOnTile(this.tilePosition.x + this.direction.x, this.tilePosition.y + this.direction.y);
-        if (player) {
-            player.damage(this.atk, this, false, true);
-        }
-        this.justAttacked = true;
+        if (player) player.damage(this.atk, this, false, true);
+        this.spikesSprite.visible = true;
         this.updateTexture();
     }
 
     updateTexture() {
         if (this.direction.x !== 0) this.scale.x = Math.abs(this.scale.x) * this.direction.x;
 
-        if (this.justAttacked) {
-            if (this.direction.x !== 0) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_x_attacked.png"].texture;
-            else if (this.direction.y === -1) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_top_attacked.png"].texture;
-            else this.texture = Game.resources["src/images/enemies/spiky_wall_trap_bottom_attacked.png"].texture;
-            this.justAttacked = false;
-        } else if (this.triggered) {
-            if (this.direction.x !== 0) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_x_triggered.png"].texture;
-            else if (this.direction.y === -1) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_top_triggered.png"].texture;
-            else this.texture = Game.resources["src/images/enemies/spiky_wall_trap_bottom_triggered.png"].texture;
+        if (this.triggered) {
+            this.texture = Game.resources["src/images/enemies/spiky_wall_trap_triggered.png"].texture;
         } else if (this.currentTurnDelay > 0) {
-            if (this.direction.x !== 0) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_x_unready.png"].texture;
-            else if (this.direction.y === -1) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_top_unready.png"].texture;
-            else this.texture = Game.resources["src/images/enemies/spiky_wall_trap_bottom_unready.png"].texture;
+            this.texture = Game.resources["src/images/enemies/spiky_wall_trap_attacked.png"].texture;
         } else {
             if (this.direction.x !== 0) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_x.png"].texture;
             else if (this.direction.y === -1) this.texture = Game.resources["src/images/enemies/spiky_wall_trap_top.png"].texture;
@@ -128,8 +100,12 @@ export class SpikyWallTrap extends Enemy {
 
     updateIntentIcon() {
         super.updateIntentIcon();
+        this.intentIcon2.visible = false;
         if (this.triggered) {
             this.intentIcon.texture = Game.resources["src/images/icons/intents/spikes.png"].texture;
+            this.intentIcon2.texture = Game.resources["src/images/icons/intents/arrow_right.png"].texture;
+            this.intentIcon2.angle = this.getArrowRightAngleForDirection(this.direction);
+            this.intentIcon2.visible = true;
         } else if (this.currentTurnDelay > 0) {
             this.intentIcon.texture = Game.resources["src/images/icons/intents/hourglass.png"].texture;
         } else {
