271b85f3981f6f6988908ca4f7cb7668cba306c0
sunDalik <dali1999@mail.ru>
2020-01-15 21:54:41 +0300

    Add amount to lying items

--- src/js/classes/equipment/lying_item.js
+++ src/js/classes/equipment/lying_item.js
@@ -3,14 +3,18 @@ import {createFloatingItemAnimation} from "../../animations";
 import {TileElement} from "../tile_elements/tile_element";
 import {swapEquipmentWithPlayer} from "../../game_logic";
 import {removeObjectFromArray} from "../../utils/basic_utils";
+import * as PIXI from "pixi.js";
+import {HUDTextStyle} from "../../drawing/draw_constants";
 
 export class LyingItem extends TileElement {
     constructor(tilePositionX, tilePositionY, item) {
         super(item.texture, tilePositionX, tilePositionY);
         this.item = item;
-        this.animation = createFloatingItemAnimation(this);
         this.width = Game.TILESIZE * 0.9;
         this.height = Game.TILESIZE * 0.9;
+        this.addAmountNumber();
+        this.place();
+        this.animation = createFloatingItemAnimation(this);
     }
 
     place() {
@@ -29,6 +33,22 @@ export class LyingItem extends TileElement {
             this.texture = this.item.texture;
             this.width = Game.TILESIZE * 0.9;
             this.height = Game.TILESIZE * 0.9;
+            this.addAmountNumber();
+        }
+    }
+
+    addAmountNumber() {
+        if (this.item.amount && this.item.amount > 1) {
+            const container = new PIXI.Container();
+            const text = new PIXI.Text("x" + this.item.amount, Object.assign({}, HUDTextStyle, {
+                fontSize: HUDTextStyle.fontSize / this.scale.x,
+                strokeThickness: HUDTextStyle.strokeThickness / this.scale.x
+            }));
+            const sprite = new PIXI.Sprite(this.item.texture);
+            container.addChild(sprite);
+            container.addChild(text);
+            text.position.set(sprite.width - text.width, 0);
+            this.texture = Game.app.renderer.generateTexture(container);
         }
     }
 }
