a1b201c493f8f9a13a5555706804eff9ed804e44
sunDalik <dali1999@mail.ru>
2020-04-07 16:15:24 +0300

    Allow spear to attack in close combat with 0.25 atk

--- src/js/classes/equipment/weapons/spear.js
+++ src/js/classes/equipment/weapons/spear.js
@@ -1,9 +1,9 @@
 import {Game} from "../../../game"
-import {ENEMY_TYPE, EQUIPMENT_TYPE, RARITY, WEAPON_TYPE} from "../../../enums";
+import {EQUIPMENT_TYPE, RARITY, WEAPON_TYPE} from "../../../enums";
 import {isEnemy, isLit, isRelativelyEmpty} from "../../../map_checks";
-import {createPlayerAttackTile, createWeaponAnimationClub, createWeaponAnimationStab} from "../../../animations";
+import {createPlayerAttackTile, createWeaponAnimationStab} from "../../../animations";
 import {WeaponsSpriteSheet} from "../../../loader";
-import {statueLeftHandPoint, statueRightHandPoint} from "../../inanimate_objects/statue";
+import {statueLeftHandPoint} from "../../inanimate_objects/statue";
 
 export class Spear {
     constructor() {
@@ -17,29 +17,23 @@ export class Spear {
     }
 
     attack(wielder, dirX, dirY) {
-        const tileX1 = wielder.tilePosition.x + dirX;
-        const tileY1 = wielder.tilePosition.y + dirY;
+        const attackTileX1 = wielder.tilePosition.x + dirX;
+        const attackTileY1 = wielder.tilePosition.y + dirY;
         const attackTileX2 = wielder.tilePosition.x + dirX * 2;
         const attackTileY2 = wielder.tilePosition.y + dirY * 2;
         const atk = wielder.getAtkWithWeapon(this);
-        if (isEnemy(attackTileX2, attackTileY2) && isRelativelyEmpty(tileX1, tileY1) && isLit(attackTileX2, attackTileY2)) {
+        if (isEnemy(attackTileX2, attackTileY2) && isRelativelyEmpty(attackTileX1, attackTileY1) && isLit(attackTileX2, attackTileY2)) {
             createWeaponAnimationStab(wielder, this, dirX * 2, dirY * 2, 8, 4, 1.1, true);
             createPlayerAttackTile({x: attackTileX2, y: attackTileY2});
             Game.map[attackTileY2][attackTileX2].entity.damage(wielder, atk, dirX, dirY, false);
             return true;
-        } else if (isEnemy(tileX1, tileY1)) {
-            const enemy = Game.map[tileY1][tileX1].entity;
-            if (enemy.type === ENEMY_TYPE.SPIDER || enemy.type === ENEMY_TYPE.SPIDER_GRAY
-                || enemy.type === ENEMY_TYPE.SPIDER_GREEN || enemy.type === ENEMY_TYPE.SPIDER_RED
-                || enemy.type === ENEMY_TYPE.ROLLER_RED && dirX !== 0) {
-                if (enemy.stun < 2) {
-                    createWeaponAnimationClub(wielder, this, dirX, dirY, 8, 4, 60, 1.1, 0, true);
-                    createPlayerAttackTile({x: tileX1, y: tileY1}, 8, 0.25);
-                    enemy.stun = 2;
-                    return true
-                }
-            }
+        } else if (isEnemy(attackTileX1, attackTileY1)) {
+            createWeaponAnimationStab(wielder, this, dirX, dirY, 10, 5, 1.1, true);
+            createPlayerAttackTile({x: attackTileX1, y: attackTileY1});
+            Game.map[attackTileY1][attackTileX1].entity.damage(wielder, 0.25, dirX, dirY, false);
+            return true;
         }
+
         return false;
     }
 
