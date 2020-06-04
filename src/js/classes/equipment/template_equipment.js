84dba37cc6168cd510d9ca1f65bfa649b1fc0ced
sunDalik <dali1999@mail.ru>
2020-06-05 00:28:38 +0300

    Resprite all armor, wiz hat, seer circlet

--- src/js/classes/equipment/template_equipment.js
+++
@@ -1,9 +0,0 @@
-import {EQUIPMENT_TYPE, RARITY} from "../../enums";
-
-export class TemplateEquipment {
-    constructor() {
-        this.nonremoveable = true;
-        this.infinite = true;
-        this.passive = true;
-    }
-}
