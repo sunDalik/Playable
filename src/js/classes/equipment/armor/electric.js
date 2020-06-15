e9ab8642af6c9a73173222d93b6df672c9d1fbd7
sunDalik <dali1999@mail.ru>
2020-06-15 19:49:10 +0300

    Electric Armor: change def 0.25 -> 0.5

--- src/js/classes/equipment/armor/electric.js
+++ src/js/classes/equipment/armor/electric.js
@@ -8,10 +8,10 @@ export class ElectricArmor extends Equipment {
         this.texture = ArmorSpriteSheet["electric.png"];
         this.equipmentType = EQUIPMENT_TYPE.ARMOR;
         this.type = ARMOR_TYPE.ELECTRIC;
-        this.passiveDef = 0.25;
+        this.passiveDef = 0.5;
         this.passiveAtk = 0.25;
         this.name = "Electric Armor";
-        this.description = "+0.25 attack\n+0.25 defense\nTake half damage from electric bullets";
+        this.description = "+0.5 defense\n+0.25 attack\nTake half damage from electric bullets";
         this.rarity = RARITY.B;
         this.electricityImmunity = true;
     }
