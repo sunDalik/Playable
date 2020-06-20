9370fe3de3d75e263a6ae51904aff48e3ec665bb
sunDalik <dali1999@mail.ru>
2020-06-20 23:10:50 +0300

    Automatically activate Necromancy if player does not have free magic slots and other player is dead

--- src/js/classes/inanimate_objects/grail.js
+++ src/js/classes/inanimate_objects/grail.js
@@ -1,11 +1,12 @@
 import {Game} from "../../game";
-import {INANIMATE_TYPE, MAGIC_ALIGNMENT} from "../../enums";
+import {EQUIPMENT_ID, INANIMATE_TYPE, MAGIC_ALIGNMENT} from "../../enums";
 import {createFadingText} from "../../animations";
 import {removeItemFromPool} from "../../game_changer";
 import {GRAIL_TEXT_DARK_FILTER, GRAIL_TEXT_WHITE_FILTER} from "../../filters";
 import {InanimatesSpriteSheet} from "../../loader";
 import {ItemInanimate} from "./item_inanimate";
 import {Light} from "../equipment/magic/light";
+import {otherPlayer} from "../../utils/game_utils";
 
 export class Grail extends ItemInanimate {
     constructor(tilePositionX, tilePositionY, obelisk) {
@@ -67,18 +68,24 @@ export class Grail extends ItemInanimate {
             this.obelisk.deactivate(this);
         }
         if (this.magic) {
-            let errorMessage = "";
-            if (this.magic.alignment === MAGIC_ALIGNMENT.DARK && player === Game.player) {
-                errorMessage = "I cannot use dark magic";
-            } else if (this.magic.alignment === MAGIC_ALIGNMENT.WHITE && player === Game.player2) {
-                errorMessage = "I cannot use light magic";
-            } else if (this.magic.constructor.requiredMagic !== null && player.getMagicByConstructor(this.magic.constructor.requiredMagic) === null) {
-                errorMessage = "I don't have a required magic";
-            } else if (player.magic1 !== null && player.magic2 !== null && player.magic3 !== null) {
-                errorMessage = "I have exhausted my magic capacity";
-            }
-            if (errorMessage !== "") {
-                createFadingText(errorMessage, player.position.x, player.position.y, Game.TILESIZE / 65 * 22, 50);
+            if (this.magic.id === EQUIPMENT_ID.NECROMANCY && otherPlayer(player).dead) {
+                this.magic.cast(player);
+                removeItemFromPool(this.magic, Game.magicPool);
+                this.obelisk.deactivate(this);
+            } else {
+                let errorMessage = "";
+                if (this.magic.alignment === MAGIC_ALIGNMENT.DARK && player === Game.player) {
+                    errorMessage = "I cannot use dark magic";
+                } else if (this.magic.alignment === MAGIC_ALIGNMENT.WHITE && player === Game.player2) {
+                    errorMessage = "I cannot use light magic";
+                } else if (this.magic.constructor.requiredMagic !== null && player.getMagicByConstructor(this.magic.constructor.requiredMagic) === null) {
+                    errorMessage = "I don't have a required magic";
+                } else if (player.magic1 !== null && player.magic2 !== null && player.magic3 !== null) {
+                    errorMessage = "I have exhausted my magic capacity";
+                }
+                if (errorMessage !== "") {
+                    createFadingText(errorMessage, player.position.x, player.position.y, Game.TILESIZE / 65 * 22, 50);
+                }
             }
         }
     }
