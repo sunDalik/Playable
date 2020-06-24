b89b02f1668506dd11ce24e2de3c409dc5857dbc
sunDalik <dali1999@mail.ru>
2020-06-24 23:34:20 +0300

    Nerf Divine Bow a bit

--- src/js/classes/equipment/weapons/divine_bow.js
+++ src/js/classes/equipment/weapons/divine_bow.js
@@ -7,16 +7,19 @@ export class DivineBow extends PiercingBowLikeWeapon {
         super(WeaponsSpriteSheet["divine_bow.png"]);
         this.arrowTexture = WeaponsSpriteSheet["divine_arrow.png"];
         this.id = EQUIPMENT_ID.DIVINE_BOW;
-        //atk 1 or range 4? hmmmmm
-        this.atk = 1;
+        this.atk = 1.5;
         this.range = 4;
         this.name = "Divine Bow";
-        this.description = "Range 4\nPiercing\nAttack = 2 at full range | 1.5 at range 3 | 1 at range 2 | 0.5 at range 1";
+        this.description = "Range 4\nPiercing\nAttack = 1.5 at full range | 1.25 at range 3 | 1 at range 2 | 0.75 at range 1";
         this.rarity = RARITY.S;
     }
 
     getAtk(wielder, range) {
-        return wielder.getAtk(this, 0.5 + (range - 1) * 0.5);
+        //0.75 1 1.25 1.5
+        return wielder.getAtk(this, 0.5 + range * 0.25);
+
+        // 0.5 1 1.5 2
+        //return wielder.getAtk(this, 0.5 + (range - 1) * 0.5);
     }
 
     getArrowAnimationTime(atkOffsetX, atkOffsetY) {
