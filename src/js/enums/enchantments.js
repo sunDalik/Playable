1ad5e2a01cdbff3902a4361b9cd5f36a9a000adf
sunDalik <dali1999@mail.ru>
2020-07-29 21:59:49 +0300

    Change divine enchantment's description

--- src/js/enums/enchantments.js
+++ src/js/enums/enchantments.js
@@ -9,7 +9,7 @@ class Enchantment {
 // not entirely sure what I'm doing
 export const ENCHANTMENT_TYPE = Object.freeze({
     NONE: new Enchantment("", ""),
-    DIVINE: new Enchantment("Divine", "DIVINE: +0.5 atk, attacked enemies spread damage"),
+    DIVINE: new Enchantment("Divine", "DIVINE: +0.5 atk, attacked enemies spread damage to other enemies around"),
     CURSED: new Enchantment("Cursed", "CURSED: +1 heart, you can't take this item off"),
     NIGHTMARE: new Enchantment("Nightmare", "NIGHTMARE: killed enemies spawn ghost minions")
 });
