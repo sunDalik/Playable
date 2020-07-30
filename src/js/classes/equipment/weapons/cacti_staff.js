7a5668212fefa92da47d125e497f082ff436c9db
sunDalik <dali1999@mail.ru>
2020-07-30 14:14:20 +0300

    Reduce Cacti Staff's minions amount to 2

--- src/js/classes/equipment/weapons/cacti_staff.js
+++ src/js/classes/equipment/weapons/cacti_staff.js
@@ -11,9 +11,9 @@ export class CactiStaff extends MinionStaff {
         this.texture = WeaponsSpriteSheet["cacti_staff.png"];
         this.id = EQUIPMENT_ID.CACTI_STAFF;
         this.name = "Cacti Staff";
-        this.description = "3 cactus minions will damage enemies that stand on its tile\nMinion's atk is 1 (x2 if in WEAPON slot) and does not depend on normal atk stats";
+        this.description = "Cactus minions will damage enemies that stand on its tile\nMinion's atk is 1 (x2 if in WEAPON slot) and does not depend on normal atk stats";
         this.rarity = RARITY.B;
-        this.minions = [new CactiStaffMinion(), new CactiStaffMinion(), new CactiStaffMinion()];
+        this.minions = [new CactiStaffMinion(), new CactiStaffMinion()];
         for (const minion of this.minions) {
             minion.brothers = this.minions.filter(m => m !== minion);
         }
