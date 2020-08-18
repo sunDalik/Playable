8924d9acfd14c3ac321753d33cb9664857f58be7
sunDalik <dali1999@mail.ru>
2020-08-18 20:08:18 +0300

    Add Bomb Skull's explosion logic

--- src/js/classes/enemies/dc/bomb_skull.js
+++ src/js/classes/enemies/dc/bomb_skull.js
@@ -1,11 +1,17 @@
 import {DCEnemiesSpriteSheet} from "../../../loader";
 import {Roller} from "../fc/roller";
+import {Bomb} from "../../equipment/bag/bomb";
 
 export class BombSkull extends Roller {
     constructor(tilePositionX, tilePositionY, texture = DCEnemiesSpriteSheet["bomb_skull.png"]) {
         super(tilePositionX, tilePositionY, texture);
         this.setScaleModifier(1);
         this.slider = false;
+        this.bomber = false;
+        if (Math.random() < 0.1) {
+            this.bomber = true;
+            this.texture = DCEnemiesSpriteSheet["bomb_skull_bomber.png"];
+        }
     }
 
     rollBump() {
@@ -17,5 +23,13 @@ export class BombSkull extends Roller {
             this.correctScale();
         });
         this.direction *= -1;
+    }
+
+    die(source) {
+        super.die(source);
+
+        if (this.bomber) {
+            new Bomb().useItem(this);
+        }
     }
 }
