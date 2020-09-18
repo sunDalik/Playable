075f88b1d8d9bb43b27881bdb352f5c0bc3921ba
sunDalik <dali1999@mail.ru>
2020-09-18 15:30:14 +0300

    Make Hero Key unable to generate in shops

--- src/js/utils/pool_utils.js
+++ src/js/utils/pool_utils.js
@@ -6,22 +6,24 @@ import {randomChoice, randomShuffle} from "./random_utils";
 import {Necromancy} from "../classes/equipment/magic/necromancy";
 import {Spear} from "../classes/equipment/weapons/spear";
 import {randomlyEnchantItem} from "../game_logic";
+import {removeObjectFromArray} from "./basic_utils";
+import {HeroKey} from "../classes/equipment/accessories/hero_key";
 
-export function getRandomWeapon(canGetS = true) {
+export function getRandomWeapon(shop = false) {
     if (Game.weaponPool.length === 0) return new Spear();
-    const item = getItemFromPool(Game.weaponPool, canGetS);
+    const item = getItemFromPool(Game.weaponPool, shop);
     removeItemFromPool(item, Game.weaponPool);
     return item;
 }
 
 export function getRandomShopItem(forceWeapon = false, forceNonWeapon = false) {
-    if (forceWeapon) return getRandomWeapon(false);
-    if (forceNonWeapon) return getRandomNonWeaponItem(false);
+    if (forceWeapon) return getRandomWeapon(true);
+    if (forceNonWeapon) return getRandomNonWeaponItem(true);
 
     if (Math.random() * (Game.chestItemPool.length + Game.weaponPool.length) < Game.chestItemPool.length) {
-        return getRandomNonWeaponItem(false);
+        return getRandomNonWeaponItem(true);
     } else {
-        return getRandomWeapon(false);
+        return getRandomWeapon(true);
     }
 }
 
@@ -71,9 +73,9 @@ export function getRandomSpell() {
     else return new constructor();
 }
 
-export function getRandomNonWeaponItem(canGetS = true) {
+export function getRandomNonWeaponItem(shop = false) {
     if (Game.chestItemPool.length === 0) return new Bomb();
-    const item = getItemFromPool(Game.chestItemPool, canGetS);
+    const item = getItemFromPool(Game.chestItemPool, shop);
     removeItemFromPool(item, Game.chestItemPool);
     return item;
 }
@@ -81,10 +83,10 @@ export function getRandomNonWeaponItem(canGetS = true) {
 /**
  * Returns random item from the pool considering current rarity distribution
  * @param pool {Class[]}
- * @param canGetS {Boolean}
+ * @param shop {Boolean}
  * @returns {null|Class}
  */
-function getItemFromPool(pool, canGetS = true) {
+function getItemFromPool(pool, shop = false) {
     if (pool.length === 0) return null;
     randomShuffle(pool);
     let attempt = 0;
@@ -92,9 +94,15 @@ function getItemFromPool(pool, canGetS = true) {
     while (attempt++ < 200 && constructor === null) {
         const rand = Math.random() * 100;
         for (const rarity of [RARITY.C, RARITY.B, RARITY.A, RARITY.S]) {
-            if (rarity === RARITY.S && canGetS === false) continue;
+            if (rarity === RARITY.S && shop) continue;
             if (rand >= rarity.chanceFrom && rand <= rarity.chanceTo) {
                 const filteredPool = pool.filter(item => (new item()).rarity === rarity);
+                if (shop) {
+                    // Hero Key cannot be generated in shop
+                    // If the ONLY item in the pool of a rarity is Hero Key then we return a new bomb
+                    if (filteredPool.length === 1 && filteredPool[0] === HeroKey) constructor = Bomb;
+                    removeObjectFromArray(HeroKey, filteredPool);
+                }
                 if (filteredPool.length !== 0) constructor = randomChoice(filteredPool);
                 break;
             }
