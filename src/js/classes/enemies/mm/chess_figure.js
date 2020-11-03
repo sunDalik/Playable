da8b42863738913cdb562f79ece46725b0e4c1dc
sunDalik <dali1999@mail.ru>
2020-11-04 00:34:53 +0300

    Add bump logic of chess figures

--- src/js/classes/enemies/mm/chess_figure.js
+++ src/js/classes/enemies/mm/chess_figure.js
@@ -1,5 +1,6 @@
 import {Enemy} from "../enemy";
 import {getPlayerOnTile} from "../../../map_checks";
+import {tileDistanceDiagonal} from "../../../utils/game_utils";
 
 export class ChessFigure extends Enemy {
     constructor(texture, tilePositionX, tilePositionY) {
@@ -7,6 +8,7 @@ export class ChessFigure extends Enemy {
         this.shadowInside = true;
         this.cachedMoves = [];
         this.regenerateShadow();
+        this.sliding = true;
     }
 
     setUpChessFigure(alignment, direction) {
@@ -20,10 +22,32 @@ export class ChessFigure extends Enemy {
         const player = getPlayerOnTile(x, y);
         if (player) {
             player.damage(this.atk, this, true);
+            if (tileDistanceDiagonal(this, player, 1) === 1) {
+                this.bumpOrSlideBumpTo(x, y);
+            } else {
+                const moveDir = {x: x - this.tilePosition.x, y: y - this.tilePosition.y};
+                this.stepOrSlideTo(x - moveDir.x, y - moveDir.y);
+            }
         } else {
-            this.slide(x - this.tilePosition.x, y - this.tilePosition.y);
+            this.stepOrSlideTo(x, y);
         }
         this.cancellable = false;
+    }
+
+    stepOrSlideTo(x, y) {
+        if (this.sliding) {
+            this.slide(x - this.tilePosition.x, y - this.tilePosition.y);
+        } else {
+            this.step(x - this.tilePosition.x, y - this.tilePosition.y);
+        }
+    }
+
+    bumpOrSlideBumpTo(x, y) {
+        if (this.sliding) {
+            this.slideBump(x - this.tilePosition.x, y - this.tilePosition.y);
+        } else {
+            this.bump(x - this.tilePosition.x, y - this.tilePosition.y);
+        }
     }
 
     setStun(stun) {
--- src/js/classes/enemies/mm/pawn.js
+++ src/js/classes/enemies/mm/pawn.js
@@ -2,7 +2,7 @@ import {ENEMY_TYPE} from "../../../enums/enums";
 import {MarbleChessSpriteSheet} from "../../../loader";
 import {CHESS_ALIGNMENT} from "../bosses/marble_chess";
 import {ChessFigure} from "./chess_figure";
-import {isRelativelyEmpty} from "../../../map_checks";
+import {getPlayerOnTile, isEmpty} from "../../../map_checks";
 
 export class Pawn extends ChessFigure {
     constructor(tilePositionX, tilePositionY, texture = MarbleChessSpriteSheet["white_pawn.png"]) {
@@ -26,12 +26,25 @@ export class Pawn extends ChessFigure {
 
     getMoves(x, y) {
         const moves = [];
-        if (isRelativelyEmpty(x + this.direction.x, y + this.direction.y)) {
+        if (isEmpty(x + this.direction.x, y + this.direction.y)) {
             moves.push({x: x + this.direction.x, y: y + this.direction.y});
-        }
-        if (x === this.initialPos.x && y === this.initialPos.y) {
-            if (isRelativelyEmpty(x + this.direction.x * 2, y + this.direction.y * 2)) {
+
+            if (x === this.initialPos.x && y === this.initialPos.y
+                && isEmpty(x + this.direction.x * 2, y + this.direction.y * 2)) {
                 moves.push({x: x + this.direction.x * 2, y: y + this.direction.y * 2});
+            }
+        }
+
+        const killerTiles = [{
+            x: this.tilePosition.x + this.direction.x - this.direction.y,
+            y: this.tilePosition.y + this.direction.y - this.direction.x
+        }, {
+            x: this.tilePosition.x + this.direction.x + this.direction.y,
+            y: this.tilePosition.y + this.direction.y + this.direction.x
+        }];
+        for (const killerTile of killerTiles) {
+            if (getPlayerOnTile(killerTile.x, killerTile.y) !== null) {
+                moves.push(killerTile);
             }
         }
         return moves;
